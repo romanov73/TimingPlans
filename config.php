f90f4eaa67dbd8f6f521bb47ddfc0c3ec5280558
Anton Romanov <romanov73@gmail.com>
2018-04-09 16:23:45 +0400

    php7 fixes

--- config.php
+++ config.php
@@ -2,18 +2,18 @@
 /////////////////////////////////////////////////////////////////////
 // DB options
 /////////////////////////////////////////////////////////////////////
-define("DB_SYSTEM_HOST",   "localhost");    // DB host
-define("DB_SYSTEM_USER",   "postgres");   // system DB user
-define("DB_SYSTEM_PASS",   "");             // system DB user password
+define("DB_SYSTEM_HOST", "localhost");      // DB host
+define("DB_SYSTEM_USER", "postgres");       // system DB user
+define("DB_SYSTEM_PASS", "");               // system DB user password
 define("DB_SYSTEM_DBNAME", "timing-plans"); // DB name
 
 /////////////////////////////////////////////////////////////////////
 // Smarty template engine options
 /////////////////////////////////////////////////////////////////////
-define("SMARTY_DIR",  "libs/smarty/libs/");         // path to smarty
-define("SMARTY_TEMPLATE_DIR",  "templates/");       // smarty compiled tempaltes
-define("SMARTY_COMPILE_DIR",  "tmp/templates_c/");  // smarty compiled tempaltes
-define("DEBUG",  true);
+define("SMARTY_DIR", "libs/smarty/libs/");         // path to smarty
+define("SMARTY_TEMPLATE_DIR", "templates/");       // smarty compiled templates
+define("SMARTY_COMPILE_DIR", "tmp/templates_c/");  // smarty compiled templates
+define("DEBUG", true);
 
 define("OUTPUT_FILE_NAME", "doc.docx");
 define("WEEKS_COUNT", 18);
